167f945f7258feac4fece939b745501967b7daae
Fazal Haroon <kharoon187@gmail.com>
2023-03-28 12:58:49 +0400

    create a typescript declaration file and add stripe Publishable key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: "https://localhost:8082/api"
+    apiUrl: "https://localhost:8082/api",
+    stripePublishableKey: "pk_test_51MpXctHh3OAMCSXQBGn0dbHYK8ilJrLtlItWcdanLNRXdytZrcKFD9197qMgwQkfW1xnGq3MHUtMlDv7QbYoDRBf00syaEue02"
 //     URL no longer hard-coded in services class, We have basics in place to use other environments in the future
 };
