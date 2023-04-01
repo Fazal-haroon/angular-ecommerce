e5f9676359b9e8f05f816935480be6e5208e93bf
Fazal Haroon <kharoon187@gmail.com>
2023-04-01 16:23:56 +0400

    Update checkout.components, add a boolean field isDisabled

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -41,6 +41,8 @@ export class CheckoutComponent implements OnInit {
     cardElement: any;
     displayError: any = "";
 
+    isDisabled: boolean = false;
+
     constructor(private formBuilder: FormBuilder, private shopFormService: ShopFormService, private cartService: CartService, private checkoutService: CheckoutService, private router: Router) { // Inject our form service
     }
 
@@ -189,6 +191,8 @@ export class CheckoutComponent implements OnInit {
         //-place order
         if (!this.checkoutFormGroup.invalid && this.displayError.textContent === "") {
 
+            this.isDisabled = true;
+
             this.checkoutService.createPaymentIntent(this.paymentInfo).subscribe(
                 (paymentIntentResponse) => {
                     this.stripe.confirmCardPayment(paymentIntentResponse.client_secret,
@@ -212,6 +216,7 @@ export class CheckoutComponent implements OnInit {
                             if (result.error) {
                                 // inform the customer there was an error
                                 alert(`There was an error: ${result.error.message}`);
+                                this.isDisabled = false;
                             } else {
                                 // call REST API via the CheckoutService
                                 this.checkoutService.placeOrder(purchase).subscribe({
@@ -220,9 +225,11 @@ export class CheckoutComponent implements OnInit {
 
                                         // reset cart
                                         this.resetCart();
+                                        this.isDisabled = false;
                                     },
                                     error: err => {
                                         alert(`There was an error: ${err.message}`);
+                                        this.isDisabled = false;
                                     }
                                 })
                             }
